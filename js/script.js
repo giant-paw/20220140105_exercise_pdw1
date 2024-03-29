5aa09fd8b3a5c983296b9c9316f59bc98f18a05f
giant-paw <giantpawbusiness@gmail.com>
2024-03-29 21:25:42 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById('buttonSubmit').addEventListener('click', function() {
+    Swal.fire({
+      title: "pesanan BERHASIL",
+      text: "tunggu sms", 
+      icon: "success" 
+    });
+  });
+  
